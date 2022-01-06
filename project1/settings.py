259a165d5f9cc935028834b8a1b7503a9e50c50f
kesangChoden <97089369+kesangChoden@users.noreply.github.com>
2022-01-06 00:15:47 -0800

    Update settings.py

--- project1/settings.py
+++ project1/settings.py
@@ -129,8 +129,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-{% load staticfiles %}
-<link rel="stylesheet" href"{% static 'project1/css/main.css' %}" />
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
